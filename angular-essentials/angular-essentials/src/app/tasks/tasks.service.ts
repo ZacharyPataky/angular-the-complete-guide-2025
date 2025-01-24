c0567769fc34ead28fd7c082c4109ddf609c1574
Zachary Pataky <zacharypataky@gmail.com>
2025-01-24 16:54:52 -0500

    Chapter 2: Angular Essentials - Components, Templates, Services, & More

---
+++ angular-essentials/angular-essentials/src/app/tasks/tasks.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { DUMMY_TASKS } from '../dummy-data/dummy-tasks';
+import { NewTaskData } from '../task/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+  private tasks = DUMMY_TASKS;
+
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId);
+  }
+
+  addTask(taskData: NewTaskData, userId: string) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate,
+      userId: userId
+    });
+    this.saveTasks();
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+}
