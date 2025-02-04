67bcfa17ef44287d1c4fdd8e0c8d522d760c6f12
Zachary Pataky <zacharypataky@gmail.com>
2025-02-03 19:33:42 -0500

    Chapter 14: Routing and Building Multi-Page Single Page Applications

---
+++ routing-and-building-multi-page-single-page-applications/routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,34 @@
+import { Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterState, RouterStateSnapshot } from '@angular/router';
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink]
+})
+export class UserTasksComponent {
+  userName = input.required<string>();
+  message = input.required<string>();
+}
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName =
+    usersService.users.find(
+      u => u.id === activatedRoute.paramMap.get('userId')
+    )?.name || '';
+  return userName;
+};
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute,
+  routerState
+) => {
+  return resolveUserName(activatedRoute, routerState) + '\'s Tasks';
+};
